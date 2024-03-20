83f6ea8c7cdc958a38dbdcaeaffffa2620c8cb49
Dex9999 <1olearydec@hdsb.ca>
2024-03-19 23:18:42 -0400

    fix!

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ function getContentType(filePath) {
     }
 }
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    console.log(`Server is running on port ${port}`);
 });
